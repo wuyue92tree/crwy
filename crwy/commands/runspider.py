6fed44b14ace52102c1a8c92d3eaaed54e691011
wuyue <wuyue92tree@163.com>
2016-08-24 10:40:44 +0800

    添加日志模块

--- crwy/commands/runspider.py
+++ crwy/commands/runspider.py
@@ -4,19 +4,31 @@
 
 from __future__ import print_function
 import sys
+import os
 
+from configparser import ConfigParser
 from optparse import OptionParser
 from crwy.commands.list import Command as ListCommand
 
 
 class Command(object):
+    def get_project_settings(self):
+        conf = ConfigParser()
+        conf.read('crwy.cfg', encoding='utf-8')
+        settings = conf.get('settings', 'default').encode('utf-8')
+        return settings
+
     def main(self):
         Usage = "Usage:  crwy runspider [option] [args]"
         parser = OptionParser(Usage)
-        parser.add_option('-n', '--name', dest='name', help='spider name', metavar="NAME")
+        parser.add_option(
+            '-n', '--name', dest='name', help='spider name', metavar="NAME")
         opt, args = parser.parse_args()
 
         if len(args) < 1:
             print(Usage)
             sys.exit(1)
 
+        if opt.name is not None:
+            console = os.popen('python src/%s.py' % opt.name).read()
+            print(console)
