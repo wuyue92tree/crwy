7edb9ca7cc3d936f865b1ef2ee23207a5193708f
wuyue <wuyue92tree@163.com>
2017-02-07 12:08:52 +0800

    修改RedisQueue模块路径，新增SsdbQueue模块

---
+++ crwy/utils/queue/SsdbQueue.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# author: wuyue92tree@163.com
+
+import pyssdb
+
+
+class SsdbQueue(object):
+    """Simple Queue with SSDB Backend"""
+
+    def __init__(self, name, **ssdb_kwargs):
+        """The default connection parameters are: host='localhost', port=8888"""
+        self.__db = pyssdb.Client(**ssdb_kwargs)
+        self.key = name
+
+    def qsize(self):
+        """Return the approximate size of the queue."""
+        return self.__db.qsize(self.key)
+
+    def empty(self):
+        """Return True if the queue is empty, False otherwise."""
+        return self.qsize() == 0
+
+    def put(self, item):
+        """Put item into the queue."""
+        self.__db.qpush(self.key, item)
+
+    def get(self):
+        """Remove and return an item from the queue.
+
+        If optional args block is true and timeout is None (the default), block
+        if necessary until an item is available."""
+
+        item = self.__db.qpop(self.key)
+
+        return item
+
+    def clean(self):
+        """Empty key"""
+        return self.__db.qclear(self.key)
