1f8c88600aa2119b32a653800361699f09fba246
wuyue <wuyue92tree@163.com>
2017-12-13 16:58:40 +0800

    download_file添加参数描述，添加file_name用于指定文件保存名称

--- crwy/utils/html/html_downloader.py
+++ crwy/utils/html/html_downloader.py
@@ -18,7 +18,7 @@ class HtmlDownloader(object):
         :param url: 目标地址
         :param method: 请求方式
         :param single: 为True时，不使用session
-        :param timeout: 初始化超市时间
+        :param timeout: 初始化超时时间
         :param kwargs: 更多requests参数
         :return: 返回requests session对象
         """
@@ -33,19 +33,25 @@ class HtmlDownloader(object):
             return requests.post(url, timeout=timeout, **kwargs)
 
     def download_file(self, url, method='GET', single=False, timeout=180,
-                      save_path='./data/', **kwargs):
+                      save_path='./data/', file_name=None, **kwargs):
         """
         请求文件
         :param url: 目标地址
+        :param method: 请求方式
+        :param single: 为True时，不使用session
+        :param timeout: 初始化超时时间
         :param save_path: 保存路径
+        :param file_name: 文件名称，默认为空
+        :param kwargs: 更多requests参数
         :return: 返回保存路径
         """
-        file_name = url.split('/')[-1]
-        file = self.download(url, method=method, single=single,
-                             timeout=timeout,
-                             stream=True, **kwargs)
+        if not file_name:
+            file_name = url.split('/')[-1]
+        tmp = self.download(url, method=method, single=single,
+                            timeout=timeout,
+                            stream=True, **kwargs)
         with open(save_path + file_name, 'wb') as f:
-            for chunk in file.iter_content(chunk_size=1024):
+            for chunk in tmp.iter_content(chunk_size=1024):
                 if chunk:  # filter out keep-alive new chunks
                     f.write(chunk)
                     f.flush()
