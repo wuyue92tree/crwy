26b90c3ea05c3a7a82350ca774e3bed4e14486c0
wuyue <wuyue92tree@163.com>
2017-12-13 18:51:50 +0800

    新增exceptions模块

---
+++ crwy/exceptions.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@author: wuyue
+@contact: wuyue92tree@163.com
+@software: PyCharm
+@file: exceptions.py
+@create at: 2017-12-13 14:14
+
+这一行开始写关于本文件的说明与解释
+"""
+
+
+class CrwyException(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
+
+class CrwyCookieValidException(CrwyException):
+    pass
+
