f3bc4c981ca6cc02f066c81b5a1cc7cf7d83d684
wuyue <wuyue92tree@163.com>
2017-12-13 18:48:53 +0800

    更新logger默认采用timed_rt_logger, 基本方法添加get_cookie, 添加login_kwargs用于存放登录所需参数

--- crwy/spider.py
+++ crwy/spider.py
@@ -16,8 +16,8 @@ class SpiderBase(object):
         """
         self.html_downloader = HtmlDownloader()
         self.html_parser = HtmlParser()
-        self.logger = Logger.rt_logger()
-        self.worker = None
+        self.logger = Logger.timed_rt_logger()
+        self.login_kwargs = None    # 用于存放登录时所需的参数
         self.proxies = None
 
 
@@ -33,6 +33,9 @@ class Spider(SpiderBase):
         pass
 
     def save(self, *args, **kwargs):
+        pass
+
+    def get_cookie(self):
         pass
 
     @staticmethod
