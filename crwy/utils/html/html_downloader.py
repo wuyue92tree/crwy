861d0de23cb06167fb8f02122168465fff7d5a93
wuyue <wuyue92tree@163.com>
2017-12-13 16:50:20 +0800

    1. 下载器添加非session支持 2. downloadFile更名为download_file添加下载参数支持

--- crwy/utils/html/html_downloader.py
+++ crwy/utils/html/html_downloader.py
@@ -7,24 +7,33 @@ import requests
 
 class HtmlDownloader(object):
     """ 下载器 """
+
     def __init__(self):
         self.session = requests.session()
 
-    def download(self, url, method='GET', timeout=60, **kwargs):
+    def download(self, url, method='GET', single=False, timeout=60,
+                 **kwargs):
         """
         请求页面
         :param url: 目标地址
         :param method: 请求方式
+        :param single: 为True时，不使用session
         :param timeout: 初始化超市时间
         :param kwargs: 更多requests参数
         :return: 返回requests session对象
         """
-        if method == 'GET':
-            return self.session.get(url, timeout=timeout, **kwargs)
-        else:
-            return self.session.post(url, timeout=timeout, **kwargs)
 
-    def downloadFile(self, url, save_path='./data/'):
+        if single is False:
+            if method == 'GET':
+                return self.session.get(url, timeout=timeout, **kwargs)
+            return self.session.post(url, timeout=timeout, **kwargs)
+        else:
+            if method == 'GET':
+                return requests.get(url, timeout=timeout, **kwargs)
+            return requests.post(url, timeout=timeout, **kwargs)
+
+    def download_file(self, url, method='GET', single=False, timeout=180,
+                      save_path='./data/', **kwargs):
         """
         请求文件
         :param url: 目标地址
@@ -32,11 +41,13 @@ class HtmlDownloader(object):
         :return: 返回保存路径
         """
         file_name = url.split('/')[-1]
-        file = self.download(url, stream=True)
-        with open(save_path+file_name, 'wb') as f:
+        file = self.download(url, method=method, single=single,
+                             timeout=timeout,
+                             stream=True, **kwargs)
+        with open(save_path + file_name, 'wb') as f:
             for chunk in file.iter_content(chunk_size=1024):
                 if chunk:  # filter out keep-alive new chunks
                     f.write(chunk)
                     f.flush()
             f.close()
-        return save_path+file_name
+        return save_path + file_name
