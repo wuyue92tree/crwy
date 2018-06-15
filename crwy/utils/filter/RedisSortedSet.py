ace1adc3e23b42d2f98a9f7e5865f8c85720cc75
wuyue <wuyue92tree@163.com>
2018-06-15 18:20:55 +0800

    添加有序集合，用于采集时，进行按时间点去重

---
+++ crwy/utils/filter/RedisSortedSet.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# author: wuyue92tree@163.com
+
+from crwy.utils.sql.redis_m import get_redis_client
+
+
+class RedisSortedSet(object):
+    """Simple Deduplicate with Redis Backend"""
+
+    def __init__(self, name, namespace='deduplicate_sorted', **redis_kwargs):
+        """
+        The default connection parameters are:
+        host='localhost', port=6379, db=0
+        """
+        self.__db = get_redis_client(**redis_kwargs)
+        self.key = '%s:%s' % (namespace, name)
+
+    def zadd(self, score, item):
+        """Add item."""
+        if self.__db.zadd(self.key, score, item) == 0:
+            return False
+        else:
+            return True
+
+    def zrem(self, item):
+        """Del item."""
+        if self.__db.zrem(self.key, item) == 0:
+            return False
+        else:
+            return True
+
+    def zcard(self):
+        """Return total count."""
+        return self.__db.zcard(self.key)
+
+    def zmembers(self):
+        """Return all item."""
+        return self.__db.zmembers(self.key)
+
+    def clean(self):
+        """Empty key"""
+        return self.__db.delete(self.key)
+
+    def db(self):
+        return self.__db
