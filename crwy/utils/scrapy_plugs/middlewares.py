b7ff1957a4ca17ae0d6054aa87f1df4bc7b203aa
wuyue <wuyue92tree@163.com>
2018-09-18 18:48:26 +0800

    update cookieMiddleware logic

--- crwy/utils/scrapy_plugs/middlewares.py
+++ crwy/utils/scrapy_plugs/middlewares.py
@@ -8,6 +8,13 @@
 @create at: 2018-06-26 18:21
 
 这一行开始写关于本文件的说明与解释
+
+通过redis hash表记录站点cookie
+
+key为 cookie_pool:SITE (SITE需要在settings中指定)
+field为 账号cookie的唯一标识，可以是username，id等，具体自行约定
+value为 cookie值，必须为json格式
+
 """
 
 import json
@@ -39,6 +46,13 @@ class CookieMiddleware(RetryMiddleware):
         )
 
     def process_request(self, request, spider):
+        """
+        cookie_user不为空时，获取cookie_user对应的cookie
+        keep_cookie_user为True时，将设置cookie_user，并传递到子请求
+        :param request:
+        :param spider:
+        :return:
+        """
         if request.meta.get('cookie_user', ''):
             user = request.meta.get('cookie_user')
         else:
