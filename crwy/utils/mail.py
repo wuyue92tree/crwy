d09f73802d86b91e41e9b28581dae56689178d76
伍岳 <wuyue92tree@163.com>
2019-03-24 19:05:11 +0800

    change mail logic

--- crwy/utils/mail.py
+++ crwy/utils/mail.py
@@ -4,69 +4,117 @@
 # Email: wuyue92tree@163.com
 
 
-from __future__ import print_function
+from __future__ import print_function, unicode_literals
 
 import email
+import re
 import traceback
 import imaplib
+
 from imapclient import IMAPClient
 from email.header import decode_header
-from retrying import retry
-
 
 imaplib._MAXLINE = 10000000
 
 SEEN = br'\Seen'
 
 
-class MailReceiver(object):
-    def __init__(self, host=None, username=None, password=None,
-                 port=None, SSL=True):
-        self.server = IMAPClient(host=host, ssl=SSL, timeout=100)
-        self.username = username
-        self.password = password
+class MailReceiver(IMAPClient):
+    def __init__(self, host, timeout=60, **kwargs):
+        super(MailReceiver, self).__init__(host, timeout=timeout, **kwargs)
 
-    @retry(stop_max_attempt_number=3)
-    def folder_list(self):
-        folders = self.server.list_folders()
-
+    def get_folder_list(self):
+        """
+        获取邮箱文件夹
+        :return: list
+        """
+        folders = self.list_folders()
         res_list = []
         for folder in folders:
             if folder:
                 # print(folder[2].encode("utf-8"))
-                res_list.append(folder[2].encode("utf-8"))
+                res_list.append(folder[2])
         return res_list
 
-    @retry(stop_max_attempt_number=3)
-    def message_list(self, mailbox='INBOX', search_='all'):
-        self.server.select_folder(
-            mailbox)
+    def get_message_id_list(self, mailbox='INBOX', search_='all'):
+        """
+        获取邮件ID列表
+        :param mailbox: 邮箱文件夹
+        :param search_: 搜索规则
+        :return: list
+        """
+        self.select_folder(mailbox)
         # message_list = self.server.search('ON 21-Dec-2017')
-        message_list = self.server.search(search_)
+        message_list = self.search(search_)
         return message_list
 
-    @retry(stop_max_attempt_number=3)
-    def download_message_list(self, message_list):
-        msg_data = self.server.fetch(
-            message_list, ['INTERNALDATE', 'FLAGS', 'BODY.PEEK[]'])
-        if not msg_data:
-            # print(self.username)
-            return None, None
-        return message_list, msg_data
+    def get_message_list(self, message_id_list):
+        """
+        获取邮件列表
+        :param message_id_list: 邮件ID列表
+        :return: dict   id:email
+        """
+        message_list = self.fetch(
+            message_id_list, ['INTERNALDATE', 'FLAGS', 'BODY.PEEK[]'])
+        if not message_list:
+            return
 
-    def get_content(self, message):
+        return message_list
+
+    @staticmethod
+    def parse_email(m, flag=None):
+        """
+        解析邮件header内容
+        :param m: 原内容
+        :param flag: 解析类型标识
+        :return: 编码转换后内容
+        """
+        try:
+            res = []
+            for s, c in decode_header(m):
+                if c:
+                    res.append(s.decode(c, 'ignore'))
+                else:
+                    res.append(s.decode('utf-8') if isinstance(s, bytes) else s)
+
+            if not res:
+                return
+
+            # 处理邮件发送方 返回邮箱地址
+            if flag == 'from':
+                res = re.findall(
+                    '[0-9a-zA-Z_\.]{0,19}@[0-9a-zA-Z\.]{1,100}', res[1])
+                return res[0]
+
+            # 处理邮件接收方 返回邮箱地址列表
+            if flag == 'to':
+                new_res = []
+                for e in res[0].split(','):
+                    em = re.findall('[0-9a-zA-Z_\.]{0,19}@[0-9a-zA-Z\.]{1,100}', e)
+                    if em:
+                        new_res.append(em[0])
+
+                return new_res
+
+            return res[0]
+        except Exception as e:
+            traceback.print_exc(e)
+            return res
+
+    def get_message_content(self, message):
+        """
+        获取邮件内容
+        :param message:
+        :return:
+        """
         try:
             while True:
                 res = {}
-                msg = email.message_from_string(message['BODY[]'])
-                for s, c in decode_header(msg['subject']):
-                    try:
-                        res['subject'] = s.decode(c, 'ignore')
-                    except TypeError:
-                        res['subject'] = s
-                        # print('Subject: ' + res['subject'])
-                        # print('From: ' + email.utils.parseaddr(msg['from'])[1])
-                        # print('To: ' + email.utils.parseaddr(msg['to'])[1])
+                msg = email.message_from_bytes(message[b'BODY[]'])
+                res['subject'] = self.parse_email(msg['Subject'])
+                res['from'] = self.parse_email(msg['From'], flag='from')
+                res['to'] = self.parse_email(msg['To'], flag='to')
+                res['date'] = self.parse_email(msg['Date'])
 
                 for par in msg.walk():
                     if not par.is_multipart():
@@ -83,28 +131,27 @@ class MailReceiver(object):
                                 res['body'] = body.decode(code, 'ignore')
                             except TypeError:
                                 res['body'] = body
-
-                # print('____________________________________________')
                 return res
 
         except Exception as e:
-            traceback.print_exc()
-            # print('got msg error: %s' % e)
+            traceback.print_exc(e)
             return
 
-    def delete_message(self, messages, deleted_folder=u"Deleted Messages"):
+    def delete_message(self, messages, deleted_folder="Deleted Messages"):
+        """
+        删除邮件
+        :param messages:
+        :param deleted_folder:
+        :return:
+        """
         try:
-            self.server.add_flags(messages, SEEN)
+            self.add_flags(messages, SEEN)
             if deleted_folder:
                 # 将邮件移动到 已删除
-                self.server.copy(messages, deleted_folder)
-            self.server.delete_messages(messages)
-            self.server.expunge()
+                self.copy(messages, deleted_folder)
+            self.delete_messages(messages)
+            self.expunge()
             return True
         except Exception as e:
-            # print("邮件删除失败： %s" % e)
-            traceback.print_exc()
-            return
-
-    def close(self):
-        self.server.logout()
+            traceback.print_exc(e)
+            return False
